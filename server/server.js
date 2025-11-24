fc7e3fde1f084b819303473a16e574b03f4c32ef
KSRS CHARAN <51869689+ksrscharan@users.noreply.github.com>
2025-11-24 10:53:19 +0530

    CORS Fix + Change Create Access Token from POST to GET (#4)
    * CORS Fix + Change Create Access Token from POST to GET
    
    * Updated Package.json
    
    * Lint Fix

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 
 const authRouter = await import('./routes/auth.js');
@@ -7,6 +8,14 @@ const app = express();
 const PORT = 3000;
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    origin: 'http://localhost:5173',
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
